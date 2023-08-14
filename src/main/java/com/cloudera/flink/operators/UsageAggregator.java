c188c15f3c735b0cc26a6ef54badd2bc4c61946d
Pavel Novokshonov <pnovoksh@gmail.com>
2023-08-14 11:12:15 -0400

    Initial Commit

--- src/main/java/com/cloudera/flink/operators/UsageAggregator.java
+++ src/main/java/com/cloudera/flink/operators/UsageAggregator.java
@@ -6,24 +6,12 @@ public class UsageAggregator implements org.apache.flink.api.common.functions.Re
     @Override
     public OutputUsageMessages reduce(OutputUsageMessages value1, OutputUsageMessages value2) {
 
-        int hour1 = value1.getHourUsage();
-        int hour2 = value2.getHourUsage();
-
-   //     if (hour1 == hour2)
             return new OutputUsageMessages(
                     value1.getHourUsage(),
                     value1.getCmtshost(),
                     value1.getScn(),
                     value1.getMac(),
                     value1.getUsage() + value2.getUsage());
-
-//        else
-//            return new OutputUsageMessages(
-//                    value1.getTime(),
-//                    value1.getCmtshost(),
-//                    value1.getScn(),
-//                    value1.getMac(),
-//                    value1.getUsage());
 
     }
 }
